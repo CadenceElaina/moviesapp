d2af6e73c4880ed3b06e42ef1d35c0aa754482be
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2022-09-10 17:03:10 -0400

    updated some css

--- moviesapp/src/App.js
+++ moviesapp/src/App.js
@@ -3,53 +3,24 @@ import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
+import Home from './Home';
+import Search from "./Search";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
-  };
-
   return (
-    <div className="app">
-      <h1>MovieLand</h1>
+    <Router>
+      <Routes>
+        <Route path="/Search"
+      <Search />
+      </Route>
+      <Route path="*" element={<NotFound />}>
+      </Route>
+    </Routes>
+    </Router >
 
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
-
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-    </div>
   );
 };
 
